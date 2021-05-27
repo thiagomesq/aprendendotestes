d9cf29a1ea21176689753e066da7246b0e2a147d
Thiago Mesquita <thiago.0306@gmail.com>
2021-05-27 10:38:27 -0300

    Final Arrays e Slices

--- arrays_slices/soma.go
+++ arrays_slices/soma.go
@@ -7,3 +7,17 @@ func Soma(numeros []int) int {
 	}
 	return soma
 }
+
+func SomaTodoOResto(numerosParaSomar ...[]int) []int {
+	var somas []int
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			final := numeros[1:]
+			somas = append(somas, Soma(final))
+		}
+	}
+
+	return somas
+}
--- arrays_slices/soma_test.go
+++ arrays_slices/soma_test.go
@@ -1,8 +1,12 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSoma(t *testing.T) {
+
 	numeros := []int{1, 2, 3, 4, 5}
 
 	resultado := Soma(numeros)
@@ -11,4 +15,27 @@ func TestSoma(t *testing.T) {
 	if esperado != resultado {
 		t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
 	}
+}
+
+func TestSomaTodoOResto(t *testing.T) {
+
+	verificaSomas := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("resultado %v, esperado %v", resultado, esperado)
+		}
+	}
+
+	t.Run("faz a soma do resto", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+		verificaSomas(t, resultado, esperado)
+	})
+
+	t.Run("soma slices vazios de forma segura", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{}, []int{3, 4, 5})
+		esperado := []int{0, 9}
+		verificaSomas(t, resultado, esperado)
+	})
+
 }
