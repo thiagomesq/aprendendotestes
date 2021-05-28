dedd55c163a71c99f6962cad13bc8be2bf0d0990
Thiago Mesquita <thiago.0306@gmail.com>
2021-05-28 14:00:36 -0300

    Ponteiros e erros

---
+++ ponteiros_erros/carteira.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Carteira struct {
+	saldo Bitcoin
+}
+
+func (c *Carteira) Depositar(quantidade Bitcoin) {
+	c.saldo += quantidade
+}
+
+func (c *Carteira) Saldo() Bitcoin {
+	return c.saldo
+}
+
+var ErroSaldoInsuficiente = errors.New("não é possível retirar: saldo insuficiente")
+
+func (c *Carteira) Retirar(quantidade Bitcoin) error {
+	if quantidade > c.saldo {
+		return ErroSaldoInsuficiente
+	}
+
+	c.saldo -= quantidade
+	return nil
+}
---
+++ ponteiros_erros/go.mod
@@ -0,0 +1,3 @@
+module github.com/thiagomesq/aprendendotestes/ponteiros_erros
+
+go 1.16
